a70c727f3c8f0788adecd15cde1407ffb2f8af7e
Nadia <nadiahosisy@gmail.com>
2023-09-28 11:27:04 +0300

    added AppAprovider to the main component

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,7 +1,7 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
 const AppContext = createContext();
 
-const AppProvider = ({ children }) => {
+export const AppProvider = ({ children }) => {
   const greeting = "hello";
   return (
     <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
