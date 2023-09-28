7a3501aa1fd74ba83b61a8f86ffe1dae338cd996
Nadia <nadiahosisy@gmail.com>
2023-09-28 11:53:11 +0300

    export actions file

--- src/actions.js
+++ src/actions.js
@@ -1,1 +1,6 @@
 export const CLEAR_CART = "CLEAR_CART";
+export const REMOVE = "REMOVE";
+export const INCRESE = "INCRESE";
+export const DECREASE = "DECREASE";
+export const LOADING = "LOADING";
+export const DISPLAY_ITEMS = "DISPLAY_ITEMS";
