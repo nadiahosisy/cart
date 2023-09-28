287758ba99f9090fca59a92b5436b6cc2e7c75dd
Nadia <nadiahosisy@gmail.com>
2023-09-28 11:59:09 +0300

    import

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,5 +1,14 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
 import reducer from "../reducer";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCRESE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "../actions";
+
 const AppContext = createContext();
 
 const initialState = {
