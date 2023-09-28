ad4b4d599707333d0f934d8f83b81a727a619f08
Nadia <nadiahosisy@gmail.com>
2023-09-28 11:17:27 +0300

    added AppProvider

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,2 +1,6 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
 const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const greeting = "hello";
+};
