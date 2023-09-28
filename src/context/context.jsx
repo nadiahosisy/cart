7bc03d77ddd3b62b06ec38483e1844fd1744da30
Nadia <nadiahosisy@gmail.com>
2023-09-28 11:29:37 +0300

    added useReducer hook to my app

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -2,7 +2,7 @@ import { useContext, useReducer, useEffect, createContext } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const greeting = "hello";
+  const [] = useReducer();
   return (
     <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
   );
